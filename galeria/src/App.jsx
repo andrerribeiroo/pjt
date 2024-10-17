bec60638405a03e60ab530c46ca56d36ac6eb8a8
Andre Ribeiro <168545471+andrerribeiroo@users.noreply.github.com>
2024-10-16 21:40:58 -0300

    .

--- galeria/src/App.jsx
+++ galeria/src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Header from "../src/header/Header"; // Importa o componente Header
-import Footer from "../src/footer/Footer"; // Importa o componente Footer
-
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
     return (
-        <Header />
-
-        <Footer />
+        <>
+            <Header />
+            <Footer />
+        </>
     );
 }
 
